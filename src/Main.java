40ac5ae6980ea704db51a4e48268410e7546290c
grisha <norsulfasol@gmail.com>
2022-11-12 19:13:28 +0300

    ДЗ 1 3 "Создайте новый проект в IDEA. Создайте класс «Автомобиль», у которого есть"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car ladaGranta = new Car("Lada", "Granta", 1.7, "же" +
+                "лтый", 2015,"Россия");
+        Car audi = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черн" +
+                "ый", 2020, "Германия");
+        Car bmw = new Car("BMW", "Z8", 2.4, "", 0, "Южн" +
+                "ая Корея");
+        Car hyundai = new Car("Hyundai", "Ava" +
+                "nte", 0,  "", 2016, "Южная Корея");
+        System.out.println(ladaGranta);
+        System.out.println(audi);
+        System.out.println(bmw);
+        System.out.println(hyundai);
+
     }
 }
